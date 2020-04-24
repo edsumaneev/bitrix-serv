ca1350c134940bddfd47564ef11d5af33e86abd3
edsumaneev <edsumaneev@yandex.ru>
2020-04-24 15:41:21 +0300

    +simple modal

--- source/js/main.js
+++ source/js/main.js
@@ -122,50 +122,85 @@ $('.team-page__label').click(function () {
   };
 })();
 
+(function () {
+  window.addEventListener("DOMContentLoaded", function () {
+    function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+      else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+      }
+    }
 
-// $('.abonement__item').click(function () {
-//   var id = $(this).attr('data-tab'),
-//     content = $('.abonement__type[data-tab="' + id + '"]');
+    function template(event) {
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+      if (event.type == "blur") {
+        if (this.value.length == 2) this.value = ""
+      } else setCursorPosition(this.value.length, this)
+    }
 
-//   $('.abonement__item.active').removeClass('active');
-//   $(this).addClass('active');
+    var phoneForm = document.querySelector(".popup__input--phone");
 
-//   $('.abonement__type.active').removeClass('active');
-//   content.addClass('active');
-// });
+    phoneForm.addEventListener("input", template, false);
+    phoneForm.addEventListener("focus", template, false);
+    phoneForm.addEventListener("blur", template, false);
+  });
+})();
 
-// (function () {
-//   window.addEventListener("DOMContentLoaded", function () {
-//     function setCursorPosition(pos, elem) {
-//       elem.focus();
-//       if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-//       else if (elem.createTextRange) {
-//         var range = elem.createTextRange();
-//         range.collapse(true);
-//         range.moveEnd("character", pos);
-//         range.moveStart("character", pos);
-//         range.select()
-//       }
-//     }
+(function () {
+  var modalForm = document.querySelector(".popup");
+  var btnHeader = document.querySelector(".button__btx-header");
+  var btnCls = document.querySelector(".popup__close");
+  var scrollRmv = document.querySelector(".btx-body");
+  var modalOverlay = document.querySelector(".modal-overlay");
 
-//     function template(event) {
-//       var matrix = "+7 (___) ___ ____",
-//         i = 0,
-//         def = matrix.replace(/\D/g, ""),
-//         val = this.value.replace(/\D/g, "");
-//       if (def.length >= val.length) val = def;
-//       this.value = matrix.replace(/./g, function (a) {
-//         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-//       });
-//       if (event.type == "blur") {
-//         if (this.value.length == 2) this.value = ""
-//       } else setCursorPosition(this.value.length, this)
-//     }
+  function openModal(evt) {
+    evt.preventDefault();
+    modalForm.classList.remove("visually-hidden");
+    scrollRmv.classList.add("btx-body__scroll");
+    modalOverlay.classList.remove("visually-hidden");
+  }
 
-//     var phoneForm = document.querySelector(".feedback__phone");
+  if (btnCls) {
+    btnCls.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      modalForm.classList.add("visually-hidden");
+      scrollRmv.classList.remove("btx-body__scroll");
+      modalOverlay.classList.add("visually-hidden");
+    })
 
-//     phoneForm.addEventListener("input", template, false);
-//     phoneForm.addEventListener("focus", template, false);
-//     phoneForm.addEventListener("blur", template, false);
-//   });
-// })();
+    if (modalOverlay) {
+      modalOverlay.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        modalForm.classList.add("visually-hidden");
+        scrollRmv.classList.remove("btx-body__scroll");
+        modalOverlay.classList.add("visually-hidden");
+      })
+    }
+  }
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (modalForm) {
+        modalForm.classList.add("visually-hidden");
+        scrollRmv.classList.remove("btx-body__scroll");
+        modalOverlay.classList.add("visually-hidden");
+      }
+    }
+  });
+
+  btnHeader.addEventListener("click", openModal, false);
+
+})();
