bbaa17a4e5beff28ce90e353c5202944a4985cf2
edsumaneev <edsumaneev@yandex.ru>
2020-05-04 14:52:39 +0300

    +owl-carousel

--- source/js/portfolio.js
+++ source/js/portfolio.js
@@ -2,18 +2,19 @@
 $(document).ready(function () {
   $('.slider-portfolio').owlCarousel({
     loop: true,
-    items: 5,
+    // items: 5,
     nav: false,
     dots: false,
+    margin: 10,
     responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
       0: {
         items: 1
       },
       767: {
-        items: 2
+        items: 3
       },
       1200: {
-        items: 4
+        items: 5
       }
     }
   }).on('click', '.owl-item', function (e) {
