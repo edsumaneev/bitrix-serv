455c83c9d577da80573d89d96f91974e48c9f733
edsumaneev <edsumaneev@yandex.ru>
2020-05-06 19:27:46 +0300

    +subfilter-3

--- source/js/portfolio.js
+++ source/js/portfolio.js
@@ -1,27 +1,67 @@
 // 'use strict';
-$(document).ready(function () {
-  $('.slider-portfolio').owlCarousel({
-    loop: true,
-    // items: 5,
-    nav: false,
-    dots: false,
-    autoWidth: true,
-    // margin: 10,
-    responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-      0: {
-        items: 1
-      },
-      767: {
-        items: 3
-      },
-      1200: {
-        items: 5
-      }
+$('.slider-portfolio').slick({
+  prevArrow: false,
+  nextArrow: false,
+  dots: false,
+  infinite: true,
+  speed: 350,
+  focusOnSelect: true,
+  variableWidth: true,
+  // определяем скорость перелистывания
+  slidesToShow: 5,
+  //количество слайдов для показа
+  slidesToScroll: 1,
+  //сколько слайдов за раз перелистнется
+  responsive: [{
+    breakpoint: 1204,
+    //сообщает, при какой ширине экрана нужно включать настройки
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // centerMode: true
+      infinite: true
     }
-  }).on('click', '.owl-item', function (e) {
-    const carousel = $('.slider-portfolio').data('owl.carousel');
-    carousel.to(carousel.relative($(this).index()), false, true);
-  });
+  }, {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // centerMode: false
+    }
+  }]
+});
+
+$('.portfolio__slider').slick({
+  prevArrow: false,
+  nextArrow: false,
+  dots: false,
+  infinite: true,
+  speed: 350,
+  focusOnSelect: true,
+  // variableWidth: true,
+  centerMode: true,
+  // определяем скорость перелистывания
+  slidesToShow: 5,
+  //количество слайдов для показа
+  slidesToScroll: 1,
+  //сколько слайдов за раз перелистнется
+  responsive: [{
+    breakpoint: 1204,
+    //сообщает, при какой ширине экрана нужно включать настройки
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // centerMode: true
+      infinite: true
+    }
+  }, {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // centerMode: false
+    }
+  }]
 });
 
 $('.portfolio__label').click(function () {
@@ -40,6 +80,8 @@ $('.portfolio__label').click(function () {
   $('.portfolio__space.active').removeClass('active');
   $('.portfolio__space[data-tab="5"]').addClass('active');
   $('.portfolio__space[data-tab="10"]').addClass('active');
+  $('.portfolio__space[data-tab="20"]').addClass('active');
+  $('.portfolio__space[data-tab="27"]').addClass('active');
 });
 
 // --------
@@ -63,4 +105,30 @@ $('.portfolio__sublabel').click(function () {
 
 //   $('.team-page__listing.active').removeClass('active');
 //   content.addClass('active');
+// });
+// ----------
+// ----------
+// $(document).ready(function () {
+//   $('.slider-portfolio').owlCarousel({
+//     loop: true,
+//     // items: 5,
+//     nav: false,
+//     dots: false,
+//     autoWidth: true,
+//     // margin: 10,
+//     responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+//       0: {
+//         items: 1
+//       },
+//       767: {
+//         items: 3
+//       },
+//       1200: {
+//         items: 5
+//       }
+//     }
+//   }).on('click', '.owl-item', function (e) {
+//     const carousel = $('.slider-portfolio').data('owl.carousel');
+//     carousel.to(carousel.relative($(this).index()), false, true);
+//   });
 // });
