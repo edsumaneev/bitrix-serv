ac00d2084146af80500a6eab7bb68daa8566c425
edsumaneev <edsumaneev@yandex.ru>
2020-05-10 12:50:23 +0300

    +page detailed

--- source/js/portfolio.js
+++ source/js/portfolio.js
@@ -64,6 +64,8 @@ $('.slider-portfolio-inner').slick({
   }]
 });
 
+// клик по label - показывает набор более детальных фильтров
+// и набор карточек из первого набора .portfolio__space
 $('.portfolio__label').click(function () {
   var id = $(this).attr('data-tab'),
     content = $('.portfolio__listing[data-tab="' + id + '"]');
@@ -77,8 +79,6 @@ $('.portfolio__label').click(function () {
   $('.portfolio__sublabel.active').removeClass('active');
   $(this).addClass('active');
 
-  $('.slider-portfolio-inner').slick('refresh');
-
   $('.portfolio__space.active').removeClass('active');
   $('.portfolio__space[data-tab="5"]').addClass('active');
   $('.portfolio__space[data-tab="10"]').addClass('active');
@@ -86,51 +86,29 @@ $('.portfolio__label').click(function () {
   $('.portfolio__space[data-tab="27"]').addClass('active');
 });
 
-// --------
-$('.portfolio__sublabel').click(function () {
-  var id = $(this).attr('data-tab'),
-    content = $('.portfolio__space[data-tab="' + id + '"]');
+// клик по sublabel - показывает набор карточек .portfolio__space, связанных с последним детальным фильтром
+var clickSublabel = function () {
+  $('.portfolio__sublabel').click(function () {
+    var id = $(this).attr('data-tab'),
+      content = $('.portfolio__space[data-tab="' + id + '"]');
 
-  $('.portfolio__sublabel.active').removeClass('active');
-  $(this).addClass('active');
+    $('.portfolio__sublabel.active').removeClass('active');
+    $(this).addClass('active');
 
-  $('.portfolio__space.active').removeClass('active');
-  content.addClass('active');
+    $('.portfolio__space.active').removeClass('active');
+    content.addClass('active');
+  });
+};
+
+// перезагрузка и отображение детального фильтра "По годам"
+$('.portfolio__label--refresh').click(function () {
+
+  $('.slider-portfolio-inner').slick('refresh');
+
+  clickSublabel();
 });
-// ----------
-// $('.team-page__label').click(function () {
-//   var id = $(this).attr('data-tab'),
-//     content = $('.team-page__listing[data-tab="' + id + '"]');
 
-//   $('.team-page__label.active').removeClass('active');
-//   $(this).addClass('active');
-
-//   $('.team-page__listing.active').removeClass('active');
-//   content.addClass('active');
-// });
+// отображение детальных фильтров
+// --------
+clickSublabel();
 // ----------
-// ----------
-// $(document).ready(function () {
-//   $('.slider-portfolio').owlCarousel({
-//     loop: true,
-//     // items: 5,
-//     nav: false,
-//     dots: false,
-//     autoWidth: true,
-//     // margin: 10,
-//     responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-//       0: {
-//         items: 1
-//       },
-//       767: {
-//         items: 3
-//       },
-//       1200: {
-//         items: 5
-//       }
-//     }
-//   }).on('click', '.owl-item', function (e) {
-//     const carousel = $('.slider-portfolio').data('owl.carousel');
-//     carousel.to(carousel.relative($(this).index()), false, true);
-//   });
-// });
