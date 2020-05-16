ac4aa2601f7ed0a13559eb879193233625a03889
edsumaneev <edsumaneev@yandex.ru>
2020-05-16 13:47:47 +0300

    +phone input validator

--- build/js/portfolio.js
+++ build/js/portfolio.js
@@ -113,3 +113,40 @@ $('.portfolio__label--refresh').click(function () {
 // --------
 clickSublabel();
 // ----------
+(function () {
+  window.addEventListener("DOMContentLoaded", function () {
+    function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+      else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+      }
+    }
+
+    function template(event) {
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+      if (event.type == "blur") {
+        if (this.value.length == 2) this.value = ""
+      } else setCursorPosition(this.value.length, this)
+    }
+
+    var phoneForm = document.querySelector(".feedback__phone");
+
+    if (phoneForm) {
+      phoneForm.addEventListener("input", template, false);
+      phoneForm.addEventListener("focus", template, false);
+      phoneForm.addEventListener("blur", template, false);
+    }
+  });
+})();
